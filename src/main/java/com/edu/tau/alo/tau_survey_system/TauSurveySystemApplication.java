507b8e9f6906b1d50cbed7dc2514379cbeeb0906
kindzer2115 <kindzeryt8@gmail.com>
2026-05-13 13:07:51 +0200

    feat: initial bare spring boot project

---
+++ src/main/java/com/edu/tau/alo/tau_survey_system/TauSurveySystemApplication.java
@@ -0,0 +1,13 @@
+package com.edu.tau.alo.tau_survey_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TauSurveySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TauSurveySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/tau/alo/tau_survey_system/TauSurveySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.tau.alo.tau_survey_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TauSurveySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
